3a50ac4baa193c71db87f9beaefb19e193d8bac2
Alessio Vallero <alessio.vallero@gmail.com>
2020-01-02 09:56:03 +0100

    Feature: Intersection of two sets

---
+++ part1/week2/intersection-of-two-sets/intersections_of_two_sets.py
@@ -0,0 +1,53 @@
+from functools import cmp_to_key
+
+
+class TwoSets:
+    """
+    Given two arrays a[] and b[], each containing n distinct 2D points in the plane
+    we count the number of points that are contained both in array a[] and array b[]
+    """
+
+    @staticmethod
+    def compare(first, second):
+        if first[0] < second[0] or (first[0] == second[0] and first[1] < second[1]):
+            return -1
+        elif first[0] > second[0] or (first[0] == second[0] and first[1] > second[1]):
+            return 1
+        else:
+            return 0
+
+    @staticmethod
+    def intersection(a, b):
+        if len(a) < 1 or len(b) < 1:
+            return 0
+
+        a.sort(key=cmp_to_key(TwoSets.compare))
+        b.sort(key=cmp_to_key(TwoSets.compare))
+
+        i = j = count = 0
+        while i < len(a) and j < len(b):
+            compare_elements = TwoSets.compare(a[i], b[j])
+            if compare_elements == 0:
+                count += 1
+                i += 1
+                j += 1
+            elif compare_elements == -1:
+                i += 1
+            else:
+                j += 1
+
+        return count
+
+
+def unit_test():
+    """
+    Test TwoSets
+    """
+
+    first_ar = [[10, 1], [4, 5], [7, 2]]
+    second_ar = [[50, 4], [7, 2], [17, 2], [10, 1], [4, 5]]
+
+    print(TwoSets.intersection(first_ar, second_ar))
+
+
+unit_test()
